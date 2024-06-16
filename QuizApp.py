f71926f8cfde6cc8c3cab73de8a3eabc52baa6f5
İbrahim Yılmaz <ibrahimenes020@gmail.com>
2024-06-16 19:16:16 +0300

    questions.txt added but still needs bug fix

--- QuizApp.py
+++ QuizApp.py
@@ -1,10 +1,14 @@
 import datetime
 import re
+from random import shuffle
+
 
 def str_to_date(date_str):
-    return datetime.strptime(date_str, "%d.%m.%Y")
+    return datetime.datetime.strptime(date_str, "%d.%m.%Y")
 
-def updateStreak(filepath="Experiments/QuizApp Files/Streak&Date"):
+
+def updateStreak(filepath="QuizApp Files/Streak&Date"):
+    global streak
     today = datetime.date.today()
     with open(filepath, "r") as file:
         for line in file:
@@ -13,33 +17,69 @@ def updateStreak(filepath="Experiments/QuizApp Files/Streak&Date"):
                 streak_match = re.search(r'\d+', line)
                 streak = int(streak_match.group())
 
-            if "Date:" in line:
+            if "LastEntry:" in line:
                 date_match = re.search(r'\d{4}-\d{2}-\d{2}', line)
                 date = str_to_date(date_match.group())
 
-            if "LastEntry:" in line:
-                last_entry_match = re.search(r'\d{4}-\d{2}-\d{2}', line)
-                lastEntry = str_to_date(last_entry_match.group())
-
     difference = today - date
-    if difference > datetime.timedelta(hours=24):
+    if difference > datetime.timedelta(hours=48):
         streak = 0
-        # TODO: update streak in the file
-        # TODO: update last entry and date in the file
-    elif lastEntry - date > datetime.timedelta(hours=24):
+        updateFile(streak, date)
+    elif difference > datetime.timedelta(hours=24):
         streak += 1
-        # TODO: update streak in the file
-        # TODO: update last entry and date in the file
+        updateFile(streak, date)
 
 
+def updateFile(streak: int, lastEntry, filepath="QuizApp Files/Streak&Date"):
+    with open(filepath, "w") as file:
+        file.write("Streak:" + str(streak))
+        file.write("\nLastEntry:" + str(lastEntry))
+
+
+def read_questions(filepath="QuizApp Files/questions.txt"):
+    questions = []
+    with open(filepath, "r") as file:
+        content = file.read().strip()
+        q_and_a = content.split('---')
+        for pair in q_and_a:
+            if pair.strip():
+                question = re.search(r'Question: (.*)', pair).group(1).strip()
+                answer = re.search(r'Answer: (.*)', pair).group(1).strip()
+                questions.append((question, answer))
+    return questions
+
+
+#START OF THE MAIN APPLICATION
+updateStreak()
 
 correctAnswers = 0
 falseAnswers = 0
-questionAnswered = 0
-
-
+questionsAnswered = 0
 
 print("***Welcome to Quiz App***")
-print("Streak:", streak)
+print("Streak:", streak, "\n\n")
+
+input("Press Enter to get your first question...")
+
+questions = read_questions()
+shuffle(questions)
+for question, correct_answer in questions:
+    print("Question:", question)
+    user_answer = input("Your answer: ").strip()
+    questionsAnswered += 1
+    if user_answer.lower() == correct_answer.lower():
+        correctAnswers += 1
+        print("Correct!\n")
+    else:
+        falseAnswers += 1
+        print(f"Incorrect! The correct answer is: {correct_answer}\n")
+
+    quizEnd = input("Want to continue playing? Type 'no' if you wish to quit...")
+    if quizEnd.lower() == "no":
+        break
 
 
+print("\nQuiz Finished!")
+print(f"Questions Answered: {questionsAnswered}")
+print(f"Correct Answers: {correctAnswers}")
+print(f"Incorrect Answers: {falseAnswers}")
